f294c1612612f9461a1a955e531ce2b3dc423aba
modz2014 <dawgz1992.jp@gmail.com>
2025-03-07 00:39:24 +0800

    Update wezterm-shellextension/WezTermContextMenu/src/dllmain.cpp
    Co-authored-by: Wez Furlong <wez@wezfurlong.org>

--- wezterm-shellextension/WezTermContextMenu/src/dllmain.cpp
+++ wezterm-shellextension/WezTermContextMenu/src/dllmain.cpp
@@ -15,15 +15,9 @@ using namespace Microsoft::WRL;
 HMODULE g_hModule = nullptr;
 
 BOOL APIENTRY DllMain(HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpReserved) {
-	switch (ul_reason_for_call) {
-	case DLL_PROCESS_ATTACH:
+	if (ul_reason_for_call == DLL_PROCESS_ATTACH) {
 		g_hModule = hModule;
-		break;
-	case DLL_THREAD_ATTACH:
-	case DLL_THREAD_DETACH:
-	case DLL_PROCESS_DETACH:
-		break;
-	}
+        }
 	return TRUE;
 }
 /**
