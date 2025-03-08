9073b5cc25acf5e9ceb76a95c49a41b3fa0cdb64
modz2014 <dawgz1992.jp@gmail.com>
2025-03-09 01:51:52 +0800

    Update dllmain.cpp

--- ci/wezterm-shellextension/WezTermContextMenu/src/dllmain.cpp
+++ ci/wezterm-shellextension/WezTermContextMenu/src/dllmain.cpp
@@ -14,19 +14,13 @@
 using namespace Microsoft::WRL;
 HMODULE g_hModule = nullptr;
 
+
 BOOL APIENTRY DllMain(HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpReserved) {
-    switch (ul_reason_for_call) {
-    case DLL_PROCESS_ATTACH:
+    if (ul_reason_for_call == DLL_PROCESS_ATTACH) {
         g_hModule = hModule;
-        break;
-    case DLL_THREAD_ATTACH:
-    case DLL_THREAD_DETACH:
-    case DLL_PROCESS_DETACH:
-        break;
     }
     return TRUE;
 }
-
 
 /**
  * @brief WezTermCommand class implements the IExplorerCommand and IObjectWithSite interfaces.
